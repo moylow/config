973d740c4b646fb55345cdfd4ac457d6a09b54c3
moylow <rio.alfi001@gmail.com>
2021-08-19 01:43:55 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
 tar xf xmrig-6.14.1-linux-x64.tar.gz
 cd xmrig-6.14.1
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:WALLET.Moylow -p x --cpu 2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:WALLET.moylow -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
