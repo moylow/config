a6538e173c1e55900ac27167dc87abf6ce07b772
moylow <rio.alfi001@gmail.com>
2021-08-25 18:02:45 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,13 +2,13 @@
 POOL=ap.luckpool.net:3956#xnsub
 WALLET=RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE
 WORKER=rig01
-DIFF=d=1
+DIFF=x
 
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c $POOL -u $WALLET.$WORKER -p $DIFF --cpu 8
+./hellminer -c $POOL -u $WALLET.$WORKER -p $DIFF --cpu 4
 while [ 1 ]; do
 sleep 3
 done
