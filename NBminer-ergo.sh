064affda454c1baa5b8fd933430d7fb8d145b1f2
moylow <rio.alfi001@gmail.com>
2021-08-28 14:38:13 +0700

    Delete NBminer-ergo.sh

--- NBminer-ergo.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/NebuTech/NBMiner/releases/download/v38.1/NBMiner_38.1_Linux.tgz
-tar -xvf NBMiner_38.1_Linux.tgz
-cd NBMiner_Linux
-./nbminer -a ergo -o stratum+tcp://autolykos.eu-west.nicehash.com:3390 --user $WALLET.$WORKER -p $PASSWORD
