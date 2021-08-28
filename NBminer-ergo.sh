223f55ee083fb48c088628c18067ad5cc90bbfa8
moylow <rio.alfi001@gmail.com>
2021-08-28 14:24:12 +0700

    Update NBminer-ergo.sh

--- NBminer-ergo.sh
+++ NBminer-ergo.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-WALLET=
-WORKER=
-PASSWORD=
-
 wget https://github.com/NebuTech/NBMiner/releases/download/v38.1/NBMiner_38.1_Linux.tgz
 tar -xvf NBMiner_38.1_Linux.tgz
 cd NBMiner_Linux
