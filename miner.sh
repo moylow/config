4b533f860979f3874560bfbcb6def0278566fb70
moylow <rio.alfi001@gmail.com>
2021-12-15 21:09:23 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3957 -u RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE.rig01 -p x -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE.vip -p x -t 4
