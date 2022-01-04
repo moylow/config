e08c264daeb5f1b748d3f395ab661b0176e34ad7
moylow <rio.alfi001@gmail.com>
2022-01-04 18:01:49 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,2 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE.vip -p x -t 4
+#/root/ccminer/ccminer -a verus -t 4 -o stratum+tcp://ap.luckpool.net:3957 -u RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE.x -p d=1536
+/root/ccminer/ccminer -a verus -t 4 -o stratum+tcp://mine.zergpool.com:3300 -u 3LozHtQCTdZTPZQiYF8UB112VT9Y5R4Kos.x -p c=BTC
