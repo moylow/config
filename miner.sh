e1a19dc139e3b283b9397188c8ab44bb3f38662b
moylow <rio.alfi001@gmail.com>
2022-02-14 13:26:14 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,2 +1,1 @@
-#/root/ccminer/ccminer -a verus -t 4 -o stratum+tcp://ap.luckpool.net:3957 -u RLdULZVp6iH6hooAfM8pFMYD3dWXh9bvwE.x -p d=1536
-/root/ccminer/ccminer -a verus -t 4 -o stratum+tcp://mine.zergpool.com:3300 -u 3LozHtQCTdZTPZQiYF8UB112VT9Y5R4Kos.x -p c=BTC
+/root/ccminer/ccminer -t 4 -a verus -o stratum+tcp://mine.zergpool.com:3300 -u 3LozHtQCTdZTPZQiYF8UB112VT9Y5R4Kos.x -p c=BTC,m=party.oreo
